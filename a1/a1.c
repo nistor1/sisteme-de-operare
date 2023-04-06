21b1271edfc66924e914ef0f17c343d3aec6692a
nistor1 <gnistor.gabriel1473@gmail.com>
2023-04-06 17:42:32 +0300

    new commit

--- a1/a1.c
+++ a1/a1.c
@@ -6,10 +6,162 @@
 #include <dirent.h>
 #include <string.h>
 #include <stdbool.h>
+#include <fcntl.h>
 
 
-int listDir(const char *path, char* filter, int* firstO)
-{
+typedef struct SECTION_HEADER_{
+    char sectName[17];
+    char sectType;
+    int sectOffset;
+    int sectSize;
+}SECTION_HEADERS;
+
+typedef struct HEADER_SECTION_FILE_{
+    int version;
+    char noOfSections;
+    SECTION_HEADERS* sectionsHeaders;
+    short int headerSize;
+    char magic[2];
+
+}HEADER_SECTION_FILE;
+
+void displaySF(HEADER_SECTION_FILE headerSF) {
+    printf("SUCCESS\n");
+    printf("version=%d\n", headerSF.version);
+    printf("nr_sections=%d\n", (int)headerSF.noOfSections);
+
+    for(int i = 0; i < headerSF.noOfSections; i++) {
+        printf("section%d: %s %d %d\n", i+1, headerSF.sectionsHeaders[i].sectName, (int)headerSF.sectionsHeaders[i].sectType,  (int)headerSF.sectionsHeaders[i].sectSize);
+    }
+
+}
+
+int parseSF(char* path) {
+
+    HEADER_SECTION_FILE headerSF;
+    int fd = -1;
+    char buffer[27];
+
+    fd = open(path, O_RDONLY);
+    if(fd == -1) {
+        printf("ERROR\ninvalid directory path");
+        return -1;
+    }
+
+    lseek(fd, 0, SEEK_END);
+    lseek(fd, -26, SEEK_END);
+    
+    int step = -1;
+    int indexSections = 1;
+    while(indexSections != 0) {
+        int bufferCapacity = read(fd, buffer, 26);
+        bufferCapacity--;
+        if(bufferCapacity <= 0 || bufferCapacity > 25) {
+            printf("ERROR\n");
+            break;
+        }
+        if(bufferCapacity == 25) {     
+            step++; 
+            if(step == 0 && bufferCapacity - 2 >= 0) {
+                headerSF.magic[1] = buffer[25];
+                headerSF.magic[0] = buffer[24];
+                //printf("(%c    %c)", headerSF.magic[0], headerSF.magic[1]);
+                bufferCapacity -= 2;
+                step++;
+
+                if(headerSF.magic[1] == 'Z' && headerSF.magic[0] == 'F') { 
+                    printf("ERROR\nwrong magic|version|select_nr|sect_types");
+                    return -1;
+                }
+
+            }        
+            if(step == 1 && bufferCapacity - 2 >= 0) {
+                char tempShort[2];
+                short int f = 0;
+                tempShort[1] = buffer[23];
+                tempShort[0] = buffer[22];
+               // printf("(%d    %d)", tempShort[0], tempShort[1]);
+                memcpy(&f, tempShort, sizeof(short int));
+                //printf("%d\n", (int)f);
+                headerSF.headerSize = f;
+                bufferCapacity -= 2;
+                step++;
+            }
+            if(step == 2) {
+                lseek(fd, -headerSF.headerSize, SEEK_END);
+            }
+            if(step == 3) {
+                memcpy(&headerSF.version, buffer, sizeof(int));
+                /*if(headerSF.version >= 25 && headerSF.version <= 118) {
+                    printf("ERROR\nwrong magic|version|select_nr|sect_types");
+                    return -1;
+                } */
+                //printf("%d ", headerSF.version);
+
+                //for(int i = 0; i < 25; i++) {
+                //    printf("%d ", buffer[i]);
+                //}
+
+
+                memcpy(&headerSF.noOfSections, buffer + 4, sizeof(char));
+                if(headerSF.noOfSections < 3 || headerSF.noOfSections > 11) {
+                    printf("ERROR\nwrong magic|version|select_nr|sect_types");
+                    return -1;
+                }
+                indexSections = headerSF.noOfSections;
+                bufferCapacity -= 5;
+                lseek(fd, -headerSF.headerSize + 5, SEEK_END);
+            }
+            if(step == 4) {
+                char tempSectType = 0;
+                int tempSectOffset = 0;
+                int tempSectSize = 0;
+                if(headerSF.sectionsHeaders == NULL){
+                   headerSF.sectionsHeaders = (SECTION_HEADERS*)malloc(headerSF.noOfSections * sizeof(SECTION_HEADERS));
+                }
+                if(headerSF.sectionsHeaders == NULL) {
+                    return -1;
+                }
+                int index = headerSF.noOfSections - indexSections;
+                for(int j = 0; j < 16; j++) {
+                    headerSF.sectionsHeaders[index].sectName[j] = buffer[j];
+                    //printf("%c", buffer[j]);
+                }
+                headerSF.sectionsHeaders[index].sectName[16] = 0;
+
+                memcpy(&(tempSectType), buffer + 16, sizeof(char));
+                memcpy(&(tempSectOffset), buffer + 17, sizeof(int));
+                memcpy(&(tempSectSize), buffer + 21, sizeof(int));
+
+                if(!(tempSectType == 71 || tempSectType == 55 || tempSectType == 93 ||tempSectType == 36 ||tempSectType == 82 ||tempSectType == 67)) {
+                    printf("ERROR\nwrong magic|version|select_nr|sect_types");
+                    return -1;   
+                }
+
+                headerSF.sectionsHeaders[index].sectType = tempSectType;
+                headerSF.sectionsHeaders[index].sectOffset = tempSectOffset;
+                headerSF.sectionsHeaders[index].sectSize = tempSectSize;
+                //printf("%d\n", index);
+                //printf("->%d->%d->%d\n", headerSF.sectionsHeaders[index].sectType, headerSF.sectionsHeaders[index].sectOffset, headerSF.sectionsHeaders[index].sectSize);
+                lseek(fd, -1, SEEK_CUR);
+                indexSections--;
+                step--;
+            }
+        }
+    }
+
+    //    for(int i = 0; i < headerSF.noOfSections; i++) {
+    //    printf("section%d: %s %d %d\n", i+1, headerSF.sectionsHeaders[i].sectName, (int)headerSF.sectionsHeaders[i].sectType,  (int)headerSF.sectionsHeaders[i].sectSize);
+   // }
+
+    displaySF(headerSF);
+    free(headerSF.sectionsHeaders);
+    close(fd);
+    return 0;
+}
+
+
+int listDir(const char *path, char* filter, int* firstO) {
     DIR *dir = NULL;
     struct dirent *entry = NULL;
     dir = opendir(path);
@@ -62,7 +214,7 @@ int listDir(const char *path, char* filter, int* firstO)
     }  
     closedir(dir);
     return 0;
-}
+    }   
 
     void listRec(const char *path, char* filter, int* firstO) {
         DIR *dir = NULL;
@@ -95,22 +247,23 @@ int listDir(const char *path, char* filter, int* firstO)
         closedir(dir);
     }
 
+
 int main(int argc, char **argv){
     if(argc >= 2){
         if(strcmp(argv[1], "variant") == 0){
             printf("19799\n");
         } else {
             bool list = false;
-            //bool parse = false;
+            bool parse = false;
             for (int i = 1; i < argc; i++) {
                 if (strcmp(argv[i], "list") == 0) {
                     list = true;
                     break;
                 }
-              //  if (strcmp(argv[i], "parse") == 0) {
-               //     parse = true;
-                 //   break;
-                //}
+                if (strcmp(argv[i], "parse") == 0) {
+                    parse = true;
+                    break;
+                }
             }
             if(list == true) {
                 bool isRecursive = false;
@@ -150,7 +303,16 @@ int main(int argc, char **argv){
                 } else {
                     listDir(path, filter, &firstO);
                 } 
-            } 
+            } else if(parse == true) {
+                char* path = NULL;
+                for (int i = 1; i < argc; i++) {
+                    if (strncmp(argv[i], "path=", 5) == 0) {
+                        path = argv[i] + 5;
+                        break;
+                    }
+                }
+                parseSF(path);
+            }
         }
     } 
     
